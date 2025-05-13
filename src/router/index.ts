a9fe1692a022f2419e697aa84efc82067926e80b
DenisSTK <denisdrawler@gmail.com>
2025-05-13 20:02:48 +0300

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -39,6 +39,7 @@ router.beforeEach(async (to, _, next) => {
         }
 
         const isAuth = await checkAuth();
+        addError(isAuth)
         if ((to.name === 'log-in' || to.name === 'sign-up') && isAuth) {
             return next('/c');
         }
